26743ad3468d5898870175d6c39d9013595a0304
Pratyush <itispriyanshu@gmail.com>
2021-01-17 01:02:18 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -7,8 +7,8 @@ except:
         IS_VPS = False
         API_HASH = "aaea06c47290c6e551d851d4844469e6"
         API_ID = "2280058"
-        BOT_TOKEN = "1553742464:AAEZjNplspJGrL6PQMnj3lbvWyfIb8ciYpo"
-        BASE_URL_OF_BOT = "http://t.me/TheSpotifyRobot"
+        BOT_TOKEN = "1520379734:AAHcqBrDGy_MeCrrCyrry5s0tiXkN04A9go"
+        BASE_URL_OF_BOT = "http://torrentRobot.zeet.co"
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
         ALD_USR = [1147526472]
         
@@ -16,16 +16,16 @@ except:
         EDIT_SLEEP_SECS = 40
 
         # Telegram Upload Limit (in bytes)
-        TG_UP_LIMIT = 1900000000
+        TG_UP_LIMIT = 1800000000
 
         # Should force evething uploaded into Document
         FORCE_DOCUMENTS = False
 
         # Chracter to use as a completed progress 
-        COMPLETED_STR = "▰"
+        COMPLETED_STR = "🌑"
 
         # Chracter to use as a incomplete progress
-        REMAINING_STR = "▱"
+        REMAINING_STR = "⚪"
 
         # DB URI for access
         DB_URI = "postgres://abdzbgwltfziwy:016aea57b883362ef1305b5d340f2ea83dcdf4e958e71f5cedbb020a72b5d81f@ec2-54-157-234-29.compute-1.amazonaws.com:5432/d8j8aptt91frmi"
@@ -51,10 +51,10 @@ except:
         DEF_RCLONE_DRIVE = ""
 
         # Max size of the videos in playlist allowed
-        MAX_YTPLAYLIST_SIZE = 500
+        MAX_YTPLAYLIST_SIZE = 200
         
         # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 200
+        MAX_TORRENT_SIZE = 30
 
         # This is to stop someone from abusing the system by imposing the limit
         # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
